da2b34f4c1e56c6d4881ee3d5db9b663a0227e0e
sorenmat <sorenmat>
2005-11-28 15:17:14 +0000

    Removed sysout

--- JIndex/gui/MailGUI.java
+++ JIndex/gui/MailGUI.java
@@ -17,26 +17,26 @@ import utils.JStringUtils;
 /**
  * @author sorenm
  */
-public class MailGUI extends MainContentsGUI  {
+public class MailGUI extends MainContentsGUI {
 	Document doc;
-	
+
 	public MailGUI(Document _doc) {
 		super(_doc);
 		doc = _doc;
 		setOpenAction(doc.get("path"));
-		setOpenAction("evolution"); 
+		setOpenAction("evolution");
 
-//				" \"email://local@local/Inbox;uid="+doc.get("uid")+"\"");
+		// " \"email://local@local/Inbox;uid="+doc.get("uid")+"\"");
 	}
+
 	public String getTextContent() {
 		String textstring = "Wrong mimeencoding";
 		try {
-			System.out.println(MimeUtility.decodeText(doc.get("from").trim()));
-		String from = JStringUtils.encodeXMLEntities(MimeUtility.decodeText(doc.get("from").trim()));
-		String subject = JStringUtils.encodeXMLEntities(MimeUtility.decodeText(doc.get("subject")));
-		textstring = "<span font_desc=\"sans bold 10\">" + from + "</span>\n";
-		textstring += subject;
-		
+			String from = JStringUtils.encodeXMLEntities(MimeUtility.decodeText(doc.get("from").trim()));
+			String subject = JStringUtils.encodeXMLEntities(MimeUtility.decodeText(doc.get("subject")));
+			textstring = "<span font_desc=\"sans bold 10\">" + from + "</span>\n";
+			textstring += subject;
+
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
@@ -45,21 +45,21 @@ public class MailGUI extends MainContentsGUI  {
 	}
 
 	public String[] getOpenAction() {
-		String[] value = new String[2]; 
+		String[] value = new String[2];
 		value[0] = "evolution";
-		value[1] = "email://local@local/Inbox;uid="+doc.get("uid");
+		value[1] = "email://local@local/Inbox;uid=" + doc.get("uid");
 		return value;
 	}
 
-//	public byte[] getIcon() {
-//		String type = doc.get("type");
-//		type =type = StringUtils.replace(type,"/","-");
-//		File f = new File("images/mimetypes/gnome-mime-"+type+".png");
-//		try {
-//			return FileUtility.getBytesFromFile(f);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		return null;
-//	}
+	// public byte[] getIcon() {
+	// String type = doc.get("type");
+	// type =type = StringUtils.replace(type,"/","-");
+	// File f = new File("images/mimetypes/gnome-mime-"+type+".png");
+	// try {
+	// return FileUtility.getBytesFromFile(f);
+	// } catch (IOException e) {
+	// e.printStackTrace();
+	// }
+	// return null;
+	// }
 }
